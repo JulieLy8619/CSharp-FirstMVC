d2ff6ee3956c7670b95ddbdbf8235513a2d3adcc
juliely8619 <july8619@yahoo.com>
2019-01-23 10:08:27 -0800

    We have a home page, yay

--- lab11FirstMVC/Startup.cs
+++ lab11FirstMVC/Startup.cs
@@ -15,20 +15,29 @@ namespace lab11FirstMVC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseMvc(routes =>
+            { 
+           routes.MapRoute(
+               name: "default",
+               template: "{controller=Home}/{action=Index}/{id?}");
             });
+            app.UseStaticFiles();
+            //I think she said we don't need these...
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
